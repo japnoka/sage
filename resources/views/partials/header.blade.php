5d576163572d046359ebb4aebf438275379987b0
Muhammadsidiq <japnoka@mail.ru>
2021-02-15 15:12:32 +0300

    thme sage

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,67 @@
+<!--Google fonts: Roboto-->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+
+<header class="banner">
+  <div class="container">
+    <div class="top-nav d-flex justify-content-between py-2 align-items-center">
+      <div class="d-inline-flex align-items-center">
+        <div class="logo align-self-center">
+          <a href="#"><img src="http://test.stomatologiakazan.ru/app/uploads/2021/02/logo1.png" alt="LOGO"></a>
+        </div>
+        <div class="adress d-none d-lg-block ">
+          г.Казань, ул.Серова,48
+        </div>
+        <div class="price d-none d-lg-block">
+          <a href="#">Скачать прайс</a>
+        </div>
+      </div>
+      <div class="button">
+        <button class="btn shadow" type="button">Записаться на прием</button>
+      </div>
+    </div>
+  </div>
+  <!--Navbar -->
+  <div class="bg-white shadow">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+          
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Главная</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Цены</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">О клинике</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Акции</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Блог</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Вопросы врачу</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Контакты</a>
+          </li>  
+        </ul>
+        <div class="d-flex">
+          <a class="tel" href="tel:+7 929 725 61 14">+7 929 725 61 14</a>
+        </div>
+        <div class="d-lg-none d-block w-100">
+          <div class="pt-2 w-100">г. Якутск, Ленина 40</div>
+          <a href="#"><div class="pt-2 w-100">Скачать прайс</div></a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</div>
+</header>
